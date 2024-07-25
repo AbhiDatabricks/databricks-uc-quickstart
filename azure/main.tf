3b4c120e72ef16a06d7d7f79a872f7677abba942
Junchi Liu <junchi_liu@hotmail.com>
2024-07-25 13:54:09 +1000

    Adding tenant ID to user provider auth

--- azure/main.tf
+++ azure/main.tf
@@ -69,34 +69,37 @@ module "prod_catalog" {
 
 // 
 module "prod_sp_group" {
-  source                = "./modules/users"
-  group_name            = "${local.prefix}-${var.group_1}"
-  databricks_account_id = var.databricks_account_id
+  source                        = "./modules/users"
+  group_name                    = "${local.prefix}-${var.group_1}"
+  databricks_account_id         = var.databricks_account_id
   # databricks_account_username = var.databricks_account_username
   # databricks_account_password = var.databricks_account_password
   azure_client_id               = var.azure_client_id
   azure_client_secret           = var.azure_client_secret
-  databricks_workspace_id = var.databricks_workspace_id
+  databricks_workspace_id       = var.databricks_workspace_id
+  azure_tenant_id               = var.azure_tenant_id
 }
 
 module "developers_group" {
-  source                = "./modules/users"
-  group_name            = "${local.prefix}-${var.group_2}"
-  databricks_account_id = var.databricks_account_id
+  source                        = "./modules/users"
+  group_name                    = "${local.prefix}-${var.group_2}"
+  databricks_account_id         = var.databricks_account_id
   # databricks_account_username = var.databricks_account_username
   # databricks_account_password = var.databricks_account_password
   azure_client_id               = var.azure_client_id
   azure_client_secret           = var.azure_client_secret
-  databricks_workspace_id = var.databricks_workspace_id
+  databricks_workspace_id       = var.databricks_workspace_id
+  azure_tenant_id               = var.azure_tenant_id
 }
 
 module "sandbox_users_group" {
-  source                = "./modules/users"
-  group_name            = "${local.prefix}-${var.group_3}"
-  databricks_account_id = var.databricks_account_id
+  source                        = "./modules/users"
+  group_name                    = "${local.prefix}-${var.group_3}"
+  databricks_account_id         = var.databricks_account_id
   # databricks_account_username = var.databricks_account_username
   # databricks_account_password = var.databricks_account_password
   azure_client_id               = var.azure_client_id
   azure_client_secret           = var.azure_client_secret
-  databricks_workspace_id = var.databricks_workspace_id
+  databricks_workspace_id       = var.databricks_workspace_id
+  azure_tenant_id               = var.azure_tenant_id
 }
--- azure/modules/users/variables.tf
+++ azure/modules/users/variables.tf
@@ -3,3 +3,4 @@ variable "azure_client_id" {}
 variable "azure_client_secret" {}
 variable "group_name" {}
 variable "databricks_workspace_id" {}
+variable "azure_tenant_id" {}
