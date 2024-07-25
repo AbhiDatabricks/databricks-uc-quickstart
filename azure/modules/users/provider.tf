754254f0c781a3931b55d6670c0d6d56c3dffe82
Junchi Liu <junchi_liu@hotmail.com>
2024-07-25 13:43:58 +1000

    modified host name

--- azure/modules/users/provider.tf
+++ azure/modules/users/provider.tf
@@ -9,7 +9,7 @@ terraform {
 // databricks at account level for account operations (groups)
 provider "databricks" {
   alias         = "account"
-  host          = "https://accounts.cloud.databricks.com"
+  host          = "https://accounts.azuredatabricks.net"
   client_id     = var.azure_client_id
   client_secret = var.azure_client_secret
   account_id    = var.databricks_account_id
