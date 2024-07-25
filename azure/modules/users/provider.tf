41ac1125d0ed342835c5054fd4df586dbbc2b1c6
Junchi Liu <junchi_liu@hotmail.com>
2024-07-25 14:16:54 +1000

    removing duplicate account id var

--- azure/modules/users/provider.tf
+++ azure/modules/users/provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     databricks = {
       source  = "databricks/databricks"
-      configuration_aliases = [databricks.account]
+      configuration_aliases = [ databricks.account ]
     }
   }
 }
